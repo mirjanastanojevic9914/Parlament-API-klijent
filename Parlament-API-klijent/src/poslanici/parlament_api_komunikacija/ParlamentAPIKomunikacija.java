a997476fc02cff0390927929e6d59429d2ee2cb8
mirjanastanojevic9914 <mira.95.bb@gmail.com>
2016-05-19 20:23:44 +0200

    U odgovarajucim paketima napravljene su klase Poslanik i ParlamentAPKomnikacija, sa odredjenim atributima i metodama.

---
+++ Parlament-API-klijent/src/poslanici/parlament_api_komunikacija/ParlamentAPIKomunikacija.java
@@ -0,0 +1,78 @@
+package poslanici.parlament_api_komunikacija;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import poslanici.poslanik.*;
+
+
+public class ParlamentAPIKomunikacija {
+	
+	private static final String membersURL = "http://147.91.128.71:9090/parlament/api/members";
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy.");
+	
+	public static List <Poslanik> vratiPoslanike(){
+		List<Poslanik> poslanici = new LinkedList<Poslanik>();
+		try{
+			String result = sendGet(membersURL);
+			Gson gson = new GsonBuilder().create();
+			JsonArray membersJson = gson.fromJson(result, JsonArray.class);
+			
+			for (int i = 0; i < membersJson.size(); i++) {
+				JsonObject memberJson = (JsonObject) membersJson.get(i);
+
+				Poslanik p = new Poslanik();
+				p.setId(memberJson.get("id").getAsInt());
+				p.setIme(memberJson.get("name").getAsString());
+				p.setPrezime(memberJson.get("lastName").getAsString());
+				if (memberJson.get("birthDate") != null)
+					p.setDatumRodjenja(sdf.parse(memberJson.get("birthDate").getAsString()));
+
+				poslanici.add(p);
+			}
+			
+			
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return poslanici;
+	}
+
+	private static String sendGet(String url) throws IOException {
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		con.setRequestMethod("GET");
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+		boolean endReading = false;
+		String response = "";
+
+		while (!endReading) {
+			String s = in.readLine();
+
+			if (s != null) {
+				response += s;
+			} else {
+				endReading = true;
+			}
+		}
+		in.close();
+
+		return response.toString();
+	}
+
+}
---
+++ Parlament-API-klijent/src/poslanici/poslanik/Poslanik.java
@@ -0,0 +1,70 @@
+package poslanici.poslanik;
+import java.util.Date;
+
+public class Poslanik {
+	
+	private int id;
+	private String ime;
+	private String prezime;
+	private Date datumRodjenja;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public String getPrezime() {
+		return prezime;
+	}
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+	public Date getDatumRodjenja() {
+		return datumRodjenja;
+	}
+	public void setDatumRodjenja(Date datumRodjenja) {
+		this.datumRodjenja = datumRodjenja;
+	}
+	@Override
+	public String toString() {
+		return "Poslanik [id=" + id + ", ime=" + ime + ", prezime=" + prezime
+				+ ", datumRodjenja=" + datumRodjenja + "]";
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Poslanik other = (Poslanik) obj;
+		if (datumRodjenja == null) {
+			if (other.datumRodjenja != null)
+				return false;
+		} else if (!datumRodjenja.equals(other.datumRodjenja))
+			return false;
+		if (id != other.id)
+			return false;
+		if (ime == null) {
+			if (other.ime != null)
+				return false;
+		} else if (!ime.equals(other.ime))
+			return false;
+		if (prezime == null) {
+			if (other.prezime != null)
+				return false;
+		} else if (!prezime.equals(other.prezime))
+			return false;
+		return true;
+	}
+    	
+
+}
